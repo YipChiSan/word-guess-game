8e0c23105cc1a14aa92b4ec1eb6b8b9620dbdcb1
YipChiSan <zye39@yahoo.com>
2022-06-08 22:02:56 +1000

    Add event handler to spans

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,5 +1,6 @@
 let words = ["word", "wins", "loss", "editior", "realise", "studio","long", "programmer", "newspaper", "television", "java"];
 let isGameRunning = false;
+let selectedLetter;
 
 function init() {
     let wins;
@@ -30,18 +31,45 @@ function startGame() {
 function renderRandomWord() {
     let word = words[Math.floor(Math.random()*words.length)];
     let firstNonBlank = Math.floor(Math.random() * word.length);
+    let display = document.querySelector('#display');
     let secondNonBlank;
+    let letter;
 
     do {
         secondNonBlank = Math.floor(Math.random() * word.length);
     } while (firstNonBlank === secondNonBlank);
 
     for (let i = 0; i < word.length; i++) {
+        letter = document.createElement("span");
         if (i == firstNonBlank || i == secondNonBlank) {
-
+            letter.innerHTML = word.charAt(i);
         } else {
-            
+            letter.innerHTML = "_";
         }
+        letter.addEventListener("click", spanClickHandler);
+        letter.addEventListener("keydown", spanKeyDownHandler);
+        letter.style.margin = "4px";
+        display.appendChild(letter);
+    }
+}
+
+function spanClickHandler(event) {
+    selectedLetter = event.target;
+    setInterval(function () {
+        selectedLetter.style.visibility = (selectedLetter.style.visibility == 'hidden' ? '' : 'hidden');
+    }, 500);
+}
+
+function spanKeyDownHandler(event) {
+    let dict="qwertyuiopasdfghjklzxcvbnm";
+    if(dict.includes(event.key.toLowerCase())) {
+        selectedLetter.textContent = event.key.toLowerCase();
+        selectedLetter = null;
+        clearInterval();
+    } else if (event.code == 8) {
+        selectedLetter.textContent = "_";
+        selectedLetter = null;
+        clearInterval();
     }
 
 }
