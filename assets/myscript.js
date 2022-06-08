dfae1f875fc0fd1ea5d20019ed3139172bbd3a0f
YipChiSan <zye39@yahoo.com>
2022-06-08 16:08:04 +1000

    Initialise some functions

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,3 +1,6 @@
+let words = ["word", "wins", "loss", "editior", "realise", "studio","long", "programmer", "newspaper", "television", "java"];
+let isGameRunning = false;
+
 function init() {
     let wins;
     let losses;
@@ -17,6 +20,34 @@ function init() {
 
     document.querySelector('#wins').textContent = "Wins: " + wins;
     document.querySelector('#losses').textContent = "Losses: " + losses;
+}
+
+function startGame() {
+    renderRandomWord();
+    startTimer();
+}
+
+function renderRandomWord() {
+    let word = words[Math.floor(Math.random()*words.length)];
+    let firstNonBlank = Math.floor(Math.random() * word.length);
+    let secondNonBlank;
+
+    do {
+        secondNonBlank = Math.floor(Math.random() * word.length);
+    } while (firstNonBlank === secondNonBlank);
+
+    for (let i = 0; i < word.length; i++) {
+        if (i == firstNonBlank || i == secondNonBlank) {
+
+        } else {
+            
+        }
+    }
+
+}
+
+function startTimer() {
+
 }
 
 init();
